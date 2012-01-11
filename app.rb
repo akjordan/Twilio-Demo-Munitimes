fb45d83893be6d125a06e30febe49f17ddd9c772
Andrew Jordan <andrew.k.jordan@gmail.com>
2012-01-10 16:06:27 -0800

    The app, less my SID and Authtoken

---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'sinatra'
+require 'muni'
+require 'twilio-ruby'
+
+@account_sid = ENV['SID'] || ' '
+@auth_token = ENV['TOKEN'] || ' '
+# set up a client, without any http requests
+@client = Twilio::REST::Client.new(@account_sid, @auth_token)
+@account = @client.account 
+
+get '/' do
+  response = Twilio::TwiML::Response.new do |r|
+    r.Gather :action => "input", :numDigits => 1 do
+      r.Say "Hello Andrew, are you headed to Twilio, or headed home,"
+      r.Say "Press 1 for Twilio,"
+      r.Say "Press 2 for home"
+    end
+  end
+  response.text
+end
+
+post '/input' do
+  user_input = params[:Digits]
+  
+  if user_input == "1"
+    bus = Muni::Route.find(45)
+    a = bus.inbound.stop_at("Union and Leavenworth").predictions
+    arriving_in = {:first => a.first.minutes, :second => a.second.minutes,:third => a.third.minutes,}
+    bustwo = Muni::Route.find(12)
+    b = bustwo.outbound.stop_at("Pacific and Leavenworth").predictions
+    arriving_in_2 = {:first => b.first.minutes, :second => b.second.minutes,:third => b.third.minutes,}
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "The 45 will depart in #{arriving_in[:first]}, #{arriving_in[:second]}, and #{arriving_in[:third]} minutes"
+      r.Say "The 12 will depart in #{arriving_in_2[:first]}, #{arriving_in_2[:second]}, and #{arriving_in_2[:third]} minutes"
+    end
+    response.text
+    
+  elsif user_input == "2"
+    bus = Muni::Route.find(45)
+    a = bus.outbound.stop_at("3rd and Folsom").predictions
+    arriving_in = {:first => a.first.minutes, :second => a.second.minutes,:third => a.third.minutes,}
+    bustwo = Muni::Route.find(12)
+    b = bustwo.inbound.stop_at("2nd and Howard").predictions
+    arriving_in_2 = {:first => b.first.minutes, :second => b.second.minutes,:third => b.third.minutes,}
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "The 45 will depart in #{arriving_in[:first]}, #{arriving_in[:second]}, and #{arriving_in[:third]} minutes"
+      r.Say "The 12 will depart in #{arriving_in_2[:first]}, #{arriving_in_2[:second]}, and #{arriving_in_2[:third]} minutes"
+    end
+    response.text
+  end
+  
+end
